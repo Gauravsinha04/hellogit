334f8436d603c2477107dc130564c3e122ecfe9d
Gauravsinha0604 <b.42gauravsinha@gmail.com>
2022-02-19 04:21:20 +0530

    first webpage

---
+++ index.js
@@ -0,0 +1,16 @@
+var a;
+a=10;
+console.log(a);
+
+console.log(typeof a);
+var num =1000;
+var str="abc";
+console.log(`the ${num} is ${num/2}`);
+let captainAmerica={
+    Name:"steve rogers",
+    age:"67",
+    friends:["bucky","tony","fury"],
+}
+let b=captainAmerica.Name;
+
+console.log(str.toUpperCase());
